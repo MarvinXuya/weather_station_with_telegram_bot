01b04aa04a1d1754f36297aea044933e590bf475
MarvinXuya <marvinxuya@me.com>
2020-10-18 14:33:57 -0600

    Updating data collector

--- weatherbot/weather_shadow.py
+++ weatherbot/weather_shadow.py
@@ -1,101 +1,96 @@
-# bot
+# mysql
 import json
 import MySQLdb
-# Starting weather
-import glob
-import time
+# sensors
 import get_bme280
 import get_ds18b20
+import get_si7021
+# times
 from time import sleep
 import datetime
-
-CONFIG = json.loads(open('/home/pi/weather_station_with_telegram_bot\
-/config_files/config.json', 'r').read())
-MYSQL_USER = CONFIG['mysql_user']
-MYSQL_PASSWORD = CONFIG['mysql_password']
-MYSQL_DATABASE = CONFIG['mysql_database']
-
-# logs
-
+# initial vars
+conf_p = "/home/pi/weather_station_with_telegram_bot/config_files/config.json"
+config = json.loads(open(conf_p, 'r').read())
+mysql_user = config['mysql_user']
+mysql_password = config['mysql_password']
+mysql_database = config['mysql_database']
+add_bme280 = config['bme280']
+add_ds18b20 = config['ds18b20']
+add_si7021 = config['si7021']
+sleep_time = config['sleep_time_data_collector_secs']
 # Logs
 logf = open("/var/log/mysql_chango.log", "w")
-
-
-class DS18B20(object):
-    def __init__(self):
-        self.device_file = glob.glob("/sys/bus/w1/devices/28*")[0]
-        + "/w1_slave"
-
-    def read_temp_raw(self):
-        f = open(self.device_file, "r")
-        lines = f.readlines()
-        f.close()
-        return lines
-
-    def crc_check(self, lines):
-        return lines[0].strip()[-3:] == "YES"
-
-    def read_temp(self):
-        temp_c = -255
-        attempts = 0
-
-        lines = self.read_temp_raw()
-        success = self.crc_check(lines)
-
-        while not success and attempts < 3:
-            time.sleep(.2)
-            lines = self.read_temp_raw()
-            success = self.crc_check(lines)
-            attempts += 1
-
-        if success:
-            temp_line = lines[1]
-            equal_pos = temp_line.find("t=")
-            if equal_pos != -1:
-                temp_string = temp_line[equal_pos+2:]
-                temp_c = float(temp_string)/1000.0
-        return temp_c
-
-
-port = 1
-address = 0x77  # Adafruit BME280 address. Other BME280s may be different
+# main
+logf.write("Starting data collect with values:" +
+           "\n- BME280: {}\n".format(add_bme280) +
+           "\n- DS18b20: {}\n".format(add_ds18b20) +
+           "\n- SI7021: {}\n".format(add_si7021) +
+           "\n- Sleep time: {}\n".format(sleep_time))
 while True:
-    try:
-        bme280data = get_bme280.get_bme280()
-    except Exception as e:
-        logf.write("Not able to get data from bme280: {}\n".format(e))
-    try:
-        humidity = bme280data.humidity
-    except Exception as e:
-        logf.write("Not able to assign humidity from bme280: {}\n".format(e))
-    try:
-        pressure = bme280data.pressure
-    except Exception as e:
-        logf.write("Not able to assign pressure from bme280: {}\n".format(e))
-    try:
-        ambient_temperature = bme280data.temperature
-    except Exception as e:
-        logf.write("Not able to assign temperature "
-                   + "from bme280: {}\n".format(e))
-    try:
-        ds18b20data = get_ds18b20.get_ds18b20()
-    except Exception as e:
-        logf.write("Not able to get data from ds18b20: {}\n".format(e))
-        continue
+    # sensors interaction
+    if add_si7021 is True:
+        if 'si7021data' not in vars():
+            try:
+                si7021data = get_si7021.get_si7021()
+            except AssertionError as error:
+                logf.write("Not able to get data from si7021" +
+                           ": {}\n".format(error))
+                temperature = None
+                humidity = None
+                pressure = None
+                continue
+        temperature = si7021data.temperature
+        humidity = si7021data.relative_humidity
+        pressure = None
+        add_bme280 = False
+    if add_bme280 is True:
+        if 'bme280data' not in vars():
+            try:
+                bme280data = get_bme280.get_bme280()
+            except AssertionError as error:
+                logf.write("Not able to get data from bme280" +
+                           ": {}".format(error))
+                temperature = None
+                humidity = None
+                pressure = None
+                continue
+        try:
+            humidity = bme280data.humidity
+        except AssertionError as error:
+            logf.write("Not able to assign humidity from bme280:"
+                       + " {}\n".format(error))
+            humidity = None
+        try:
+            pressure = bme280data.pressure
+        except AssertionError as error:
+            logf.write("Not able to assign pressure from bme280:"
+                       + " {}\n".format(error))
+            pressure = None
+        try:
+            temperature = bme280data.temperature
+        except AssertionError as error:
+            logf.write("Not able to assign temperature "
+                       + "from bme280: {}\n".format(error))
+            temperature = None
+    if add_si7021 is False and add_bme280 is False:
+        humidity = None
+        pressure = None
+        temperature = None
+    if add_ds18b20 is True:
+        try:
+            temperature_ds18b20 = get_ds18b20.get_ds18b20()
+        except AssertionError as error:
+            logf.write("Not able to get data from ds18b20: {}\n".format(error))
+            temperature_ds18b20 = None
+            continue
+    else:
+        temperature_ds18b20 = None
     now = datetime.datetime.now()
+    # database interaction
     try:
-        connection = MySQLdb.connect(user=MYSQL_USER,
-                                     password=MYSQL_PASSWORD,
-                                     database=MYSQL_DATABASE)
-        cursor = connection.cursor()
-        cursor.executemany(
-              """INSERT INTO botdata (date_time, humedad, presion,
-              temperatura, temperatura_DS18B20)
-              VALUES (%s, %s, %s, %s, %s)""",
-              [(now, humidity, pressure, ambient_temperature,
-                ds18b20data)])
-        connection.commit()
-        connection.close()
+        connection = MySQLdb.connect(user=mysql_user,
+                                     password=mysql_password,
+                                     database=mysql_database)
     except Exception as e:
         logf.write("Not able to connect to database: {}\n".format(str(e)))
         if (connection):
@@ -103,4 +98,22 @@ while True:
         else:
             logf.write("There is no need to close connection on database")
         continue
-    sleep(600)
+    try:
+        cursor = connection.cursor()
+        cursor.executemany(
+              """INSERT INTO botdata (date_time, humedad, presion,
+              temperatura, temperatura_DS18B20)
+              VALUES (%s, %s, %s, %s, %s)""",
+              [(now, humidity, pressure, temperature,
+                temperature_ds18b20)])
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        logf.write("Not able to insert to database: {}\n".format(str(e)))
+        if (connection):
+            connection.close()
+        else:
+            logf.write("There is no need to close connection on database")
+        continue
+    sleep(sleep_time)
